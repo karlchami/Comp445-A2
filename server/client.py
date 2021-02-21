cde4b01d16d6e77beefdd4ba833ba8db88a1891d
Karl <karljchami@gmail.com>
2021-02-20 21:07:22 -0500

    Added server args and logs

--- server/client.py
+++ server/client.py
@@ -1,7 +1,7 @@
 import socket
 
 c = socket.socket()
-
 c.connect(("localhost", 9998))
-
+nickname = input("Please enter your nickname: ")
+c.send(bytes(nickname, "utf-8"))
 print(c.recv(1024).decode())
--- server/server.py
+++ server/server.py
@@ -1,19 +1,50 @@
 import socket
+import argparse
+import logging
 
-# IPV4 and TCP by default
-s = socket.socket()
-print("Socket Created")
 
-# Bind socket to IP and port number
-s.bind(("localhost", 9998))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', help='Target port to use', required=True)
+    args = vars(parser.parse_args())
+    port = int(args['port'])
 
-# Buffer for 3 client connections
-s.listen(3)
-print("Waiting for connections")
+    logging.basicConfig(filename='server.log', level=logging.DEBUG)
+    logger = logging.getLogger()
 
-while True:
-    c, address = s.accept()
-    print("Client connected with ", address)
-    c.send(bytes("Welcome to my connection", "utf-8"))
-    c.close()
+    server = IRCServer()
+    logger.info("Server object created")
+    server.connect("localhost", port)
+    logger.info("Server connection established")
 
+    logger.info("Listening to requests")
+    while True:
+        server.receive()
+
+
+class IRCServer:
+    irc_socket = socket.socket()
+
+    def __init__(self):
+        return
+
+    def connect(self, servername, portnum):
+        self.irc_socket.bind((servername, portnum))
+        self.irc_socket.listen(9)
+
+    def close_connection(self):
+        self.irc_socket.close()
+
+    def send(self, message):
+        self.irc_socket.send(bytes(message, "utf-8"))
+
+    def receive(self):
+        client, client_address = self.irc_socket.accept()
+        response = client.recv(2040)
+        reply_message = "Welcome, " + response.decode()
+        client.send(bytes(reply_message, "utf-8"))
+        client.close()
+
+
+if __name__ == '__main__':
+    main()
